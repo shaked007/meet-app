896af5139454ec3ff1cd72a062548e6f993cfa6a
shaked007 <shakedbukai@gmail.com>
2023-05-20 16:46:52 +0300

    ff

--- src/views/loginAdmin.vue
+++ src/views/loginAdmin.vue
@@ -25,23 +25,27 @@ export default {
             locale:"he",
         logo: true // you can try false and see what happens
         });
+        //event for what happens when user enter credentials
         netlifyIdentity.on("login", async (user)=>{
             
 
             const token =  await user.jwt()
             sessionStorage.setItem("token",token)
-            this.$router.push('/admin')
-        })
-         if(!localStorage.getItem('gotrue.user')){
-            netlifyIdentity.open("login");
-            const urlArr= window.location.href
+           const urlArr= window.location.href
             if(urlArr.includes('redirect')){
                 window.location.href = urlArr.split('=')[1]
             }else{
                  this.$router.push('/admin')
 
             }
+        })
+        //if the user is not authenticated
+         if(!localStorage.getItem('gotrue.user')){
+            netlifyIdentity.open("login");
+            
+        //if the user is authenticated
         }else{
+            
             netlifyIdentity.close()
             const parsedResponse = await this.asyncParse(localStorage.getItem('gotrue.user'))
             sessionStorage.setItem('token',parsedResponse.token.access_token)
